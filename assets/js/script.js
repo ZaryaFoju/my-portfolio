3e5af3aa2e86445636a32377bd629e68fafc8f99
ZaryaFoju <157494272+ZaryaFoju@users.noreply.github.com>
2025-06-24 14:07:53 -0500

    Added Javascript functionality to recommendations

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,3 +89,65 @@ sr.reveal(".qualification__footer-text, .contact__content", {
 });
 
 sr.reveal(".qualification__footer .btn, .contact__btn", { origin: "right" });
+
+// Recommendations Slide
+
+const recommendationsSlide = new Swiper(".recommendations__wrapper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  effect: "coverflow",
+  grabCursor: true,
+  slidesPerView: 1,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    520: {
+      slidesPerView: "auto",
+    },
+  },
+});
+
+// header scroll animation
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    header.classList.add("header--scroll");
+  } else {
+    header.classList.remove("header--scroll");
+  }
+});
+
+// ScrollReveal animations
+const sr = ScrollReveal({
+  duration: 2000,
+  distance: "100px",
+  delay: 400,
+  reset: false,
+});
+
+sr.reveal(".hero__content, .about__content");
+sr.reveal(".hero__img", { origin: "top" });
+
+sr.reveal(
+  ".hero__info-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .values__card, .project__content, .recommendations__wrapper, .footer__content",
+  {
+    delay: 500,
+    interval: 100,
+  }
+);
+
+sr.reveal(".qualification__footer-text, .contact__content", {
+  origin: "left",
+});
+
+sr.reveal(".qualification__footer .btn, .contact__btn", { origin: "right" });
